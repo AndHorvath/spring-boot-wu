b21a32b5c50157173959979a9fa94fcf107f591d
András Horváth <horvath.andras@prevaudit.hu>
2022-05-21 17:38:27 +0200

    human resources project week02

--- hr/src/main/java/hu/webuni/hr/ah/model/Employee.java
+++ hr/src/main/java/hu/webuni/hr/ah/model/Employee.java
@@ -3,45 +3,45 @@ package hu.webuni.hr.ah.model;
 import java.time.LocalDateTime;
 
 public class Employee {
-	
-	// --- attributes ---------------------------------------------------------
-	
-	private long id;
-	private String name;
-	private LocalDateTime dateOfEntry;
-	private String position;
-	private int salary;
-	
-	// --- constructors -------------------------------------------------------
 
-	public Employee() { }
-	
-	public Employee(long id, String name, LocalDateTime dateOfEntry, String position, int salary) {
-		this.id = id;
-		this.name = name;
-		this.dateOfEntry = dateOfEntry;
-		this.position = position;
-		this.salary = salary;
-	}
-	
-	// --- getters and setters ------------------------------------------------
+    // --- attributes ---------------------------------------------------------
 
-	public long getId() { return id; }
-	public String getName() { return name; }
-	public LocalDateTime getDateOfEntry() {	return dateOfEntry;	}
-	public String getPosition() { return position; }
-	public int getSalary() { return salary;	}
+    private long id;
+    private String name;
+    private LocalDateTime dateOfEntry;
+    private String position;
+    private int salary;
 
-	public void setId(long id) { this.id = id; }
-	public void setName(String name) { this.name = name; }
-	public void setDateOfEntry(LocalDateTime dateOfEntry) { this.dateOfEntry = dateOfEntry; }
-	public void setPosition(String position) { this.position = position; }
-	public void setSalary(int salary) { this.salary = salary; }
+    // --- constructors -------------------------------------------------------
 
-	// --- public methods -----------------------------------------------------
+    public Employee() { }
 
-	@Override
-	public String toString() {
-		return "Employee [name=" + name + ", dateOfEntry=" + dateOfEntry + ", salary=" + salary + "]";
-	}
+    public Employee(long id, String name, LocalDateTime dateOfEntry, String position, int salary) {
+        this.id = id;
+        this.name = name;
+        this.dateOfEntry = dateOfEntry;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    // --- getters and setters ------------------------------------------------
+
+    public long getId() { return id; }
+    public String getName() { return name; }
+    public LocalDateTime getDateOfEntry() { return dateOfEntry; }
+    public String getPosition() { return position; }
+    public int getSalary() { return salary; }
+
+    public void setId(long id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setDateOfEntry(LocalDateTime dateOfEntry) { this.dateOfEntry = dateOfEntry; }
+    public void setPosition(String position) { this.position = position; }
+    public void setSalary(int salary) { this.salary = salary; }
+
+    // --- public methods -----------------------------------------------------
+
+    @Override
+    public String toString() {
+        return "Employee [name=" + name + ", dateOfEntry=" + dateOfEntry + ", salary=" + salary + "]";
+    }
 }
