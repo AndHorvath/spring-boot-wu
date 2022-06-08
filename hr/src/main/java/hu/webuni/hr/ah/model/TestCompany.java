20eb1d8fdce3755a349ac068ac048b7d4d2241fe
András Horváth <horvath.andras@prevaudit.hu>
2022-06-08 19:08:03 +0200

    human resources project week03

--- hr/src/main/java/hu/webuni/hr/ah/model/TestCompany.java
+++ hr/src/main/java/hu/webuni/hr/ah/model/TestCompany.java
@@ -9,10 +9,10 @@ public class TestCompany {
 
     public static List<Company> initializeList() {
         return new ArrayList<>(List.of(
-            new Company(1, "A-1", "CompanyA", "AddressA", TestEmployee.initializeDtoList().stream().limit(1).toList()),
-            new Company(2, "B-2", "CompanyB", "AddressB", TestEmployee.initializeDtoList().stream().limit(2).toList()),
-            new Company(3, "C-3", "CompanyC", "AddressC", TestEmployee.initializeDtoList().stream().limit(3).toList()),
-            new Company(4, "D-4", "CompanyD", "AddressD", TestEmployee.initializeDtoList().stream().limit(4).toList())
+            new Company(1, "AA-1", "CompanyA", "AddressA", TestEmployee.initializeDtoList().stream().limit(1).toList()),
+            new Company(2, "BB-2", "CompanyB", "AddressB", TestEmployee.initializeDtoList().stream().limit(2).toList()),
+            new Company(3, "CC-3", "CompanyC", "AddressC", TestEmployee.initializeDtoList().stream().limit(3).toList()),
+            new Company(4, "DD-4", "CompanyD", "AddressD", TestEmployee.initializeDtoList().stream().limit(4).toList())
         ));
     }
 
