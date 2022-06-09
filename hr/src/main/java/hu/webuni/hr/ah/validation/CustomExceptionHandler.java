f44089d01d127e18e13f1bf769f72b7651eb7504
András Horváth <horvath.andras@prevaudit.hu>
2022-06-09 02:08:48 +0200

    human resources project week04

--- hr/src/main/java/hu/webuni/hr/ah/validation/CustomExceptionHandler.java
+++ hr/src/main/java/hu/webuni/hr/ah/validation/CustomExceptionHandler.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Map;
-
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
