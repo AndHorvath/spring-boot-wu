567bc0a9d34faeeff3054ffe9dda871daf3cd03a
András Horváth <horvath.andras@prevaudit.hu>
2022-08-04 22:27:22 +0200

    human resources project week06

--- hr/src/main/java/hu/webuni/hr/ah/web/SalaryController.java
+++ hr/src/main/java/hu/webuni/hr/ah/web/SalaryController.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/salary")
